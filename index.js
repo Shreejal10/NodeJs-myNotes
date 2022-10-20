90b61a372c6a4f43e72cca951a989c429a4873a8
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-20 16:39:33 +0545

    added route for notes

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 //Available Routes
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth')) //Route for user: sigUp, logIn, getUserData
+app.use('/api/notes', require('./routes/notes')) //Route for notes: addNote, fetchAllNotes, editNotes, deleteNotes 
 
 app.listen(port, () => {
     console.log(`myNotes Backend running at  http://localhost:${port}`)
